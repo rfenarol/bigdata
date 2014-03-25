4202bca96869be4f950bba7af0f233b6e15b8c04
octawizard <roberto.manca.89@gmail.com>
2014-03-25 12:24:17 +0100

    rmv useless files

--- src/main/java/FM/hadoop/mapreduce/progetto1/MyMapper.java
+++
@@ -1,8 +0,0 @@
-package FM.hadoop.mapreduce.progetto1;
-
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.Mapper;
-
-public class MyMapper extends Mapper<Text, Text, Text, IntWritable>{
-
-}
