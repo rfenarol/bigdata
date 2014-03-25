7d14019a5286e37d595ff2229e053658c3f501fd
octawizard <roberto.manca.89@gmail.com>
2014-03-25 12:20:07 +0100

    prova a spostare tutto in una cartella

---
+++ progetto1/src/main/java/FM/hadoop/mapreduce/progetto1/MyMapper.java
@@ -0,0 +1,8 @@
+package FM.hadoop.mapreduce.progetto1;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MyMapper extends Mapper<Text, Text, Text, IntWritable>{
+
+}
---
+++ progetto1/src/main/java/FM/hadoop/mapreduce/progetto1/MyReducer.java
@@ -0,0 +1,8 @@
+package FM.hadoop.mapreduce.progetto1;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+}
