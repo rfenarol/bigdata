af1888ada81e0de9a1264bce9cf0597a0e2bc39c
rfenarol <robertofenaroli@gmail.com>
2014-03-25 13:16:37 +0100

    first commit esercizio 1

---
+++ esercizio1/src/main/java/it/bigdata/hadoop/esercizio1/App.java
@@ -0,0 +1,13 @@
+package it.bigdata.hadoop.esercizio1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
