ee628389bdd8626a3dbc2335f4cc4e6d50f82b07
octawizard <roberto.manca.89@gmail.com>
2014-03-25 13:03:45 +0100

    rmv schifo

--- progetto1/src/main/java/FM/hadoop/mapreduce/progetto1/MyReducer.java
+++
@@ -1,8 +0,0 @@
-package FM.hadoop.mapreduce.progetto1;
-
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.Reducer;
-
-public class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-}
