793161565999cd4fe42f40aea4d181884915bd2b
octawizard <roberto.manca.89@gmail.com>
2014-03-27 15:58:56 +0100

    update es2 structure

---
+++ esercizio2/src/main/java/it/bigdata/hadoop/esercizio2/Mapper.java
@@ -0,0 +1,7 @@
+package it.bigdata.hadoop.esercizio2;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class Mapper extends Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT> {
+
+}
---
+++ esercizio2/src/main/java/it/bigdata/hadoop/esercizio2/Reducer.java
@@ -0,0 +1,7 @@
+package it.bigdata.hadoop.esercizio2;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Reducer extends Reducer<KEYIN, VALUEIN, KEYOUT, VALUEOUT> {
+
+}
