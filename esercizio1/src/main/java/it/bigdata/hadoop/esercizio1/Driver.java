ebca22f405a2b900afe801d748cde368cb553e1f
octawizard <roberto.manca.89@gmail.com>
2014-03-29 12:23:06 +0100

    update comment to explain how to chain multiple mapreduce jobs

--- esercizio1/src/main/java/it/bigdata/hadoop/esercizio1/Driver.java
+++ esercizio1/src/main/java/it/bigdata/hadoop/esercizio1/Driver.java
@@ -34,26 +34,52 @@ public class Driver {
 										// for it to finish.
 										// Se il parametro √® true, print the
 										// progress to the user
-		
-		
+
+		/*
+		 * Secondo me la soluzione per fare l'ordinamento Ë questa: 
+		 * 1 Chaining MapReduce jobs in a sequence 
+		 * Though you can execute the two jobs
+		 * manually one after the other, itís more convenient to automate the
+		 * execution sequence. You can chain MapReduce jobs to run sequen≠
+		 * tially, with the output of one MapReduce job being the input to the
+		 * next. Chaining MapReduce jobs is analogous to Unix pipes .
+		 * mapreduce-1 | mapreduce-2 | mapreduce-3 | ...
+		 * 
+		 * Chaining MapReduce jobs sequentially is quite straightforward. Recall
+		 * that a driver sets up a JobConf object with the configuration
+		 * parameters for a MapReduce job and passes the JobConf object to
+		 * JobClient.runJob() to start the job. As Job≠ Client.runJob() blocks
+		 * until the end of a job, chaining MapReduce jobs involves calling the
+		 * driver of one MapReduce job after another. The driver at each job
+		 * will have to create a new JobConf object and set its input path to be
+		 * the output path of the previous job. You can delete the intermediate
+		 * data generated at each step of the chain at the end.
+		 */
+
 		/* da qui in poi √® un tentativo */
-//		
-//		Job job2 = Job.getInstance(new Configuration(), "esercizio1.1 chain");
-//		
-//		job.setSortComparatorClass(MySchifo.class); //ordinamento decrescente nella fase di shuffle&sort
-//		
-//		job2.setMapperClass(Mapper.class); //questo mapper dovr√† solo invertire k,v in v,k
-//		job2.setReducerClass(Reducer.class); //non deve fare niente a parte riportare in forma k,v context.write(words, keySum);
-//		
-//		/* output del tipo (testo, intero) */
-//		job.setOutputKeyClass(Text.class); // Set the key class for the job output data.
-//		job.setOutputValueClass(IntWritable.class); // Set the value class for the job output data.
-//
-//		job.waitForCompletion(true); // Submit the job to the cluster and wait
-//										// for it to finish.
-//										// Se il parametro √® true, print the
-//										// progress to the user
-//		
+		//
+		// Job job2 = Job.getInstance(new Configuration(),
+		// "esercizio1.1 chain");
+		//
+		// job.setSortComparatorClass(MySchifo.class); //ordinamento decrescente
+		// nella fase di shuffle&sort
+		//
+		// job2.setMapperClass(Mapper.class); //questo mapper dovr√† solo
+		// invertire k,v in v,k
+		// job2.setReducerClass(Reducer.class); //non deve fare niente a parte
+		// riportare in forma k,v context.write(words, keySum);
+		//
+		// /* output del tipo (testo, intero) */
+		// job.setOutputKeyClass(Text.class); // Set the key class for the job
+		// output data.
+		// job.setOutputValueClass(IntWritable.class); // Set the value class
+		// for the job output data.
+		//
+		// job.waitForCompletion(true); // Submit the job to the cluster and
+		// wait
+		// // for it to finish.
+		// // Se il parametro √® true, print the
+		// // progress to the user
+		//
 	}
 }
-		
