d94e5e36ffcc00285adf0d71a9217967f3552db4
octawizard <roberto.manca.89@gmail.com>
2014-04-02 19:05:01 +0200

    update Reducer alghoritm

--- esercizio2/src/main/java/it/bigdata/hadoop/esercizio2/UsersCoupleReducer.java
+++ esercizio2/src/main/java/it/bigdata/hadoop/esercizio2/UsersCoupleReducer.java
@@ -1,7 +1,8 @@
 package it.bigdata.hadoop.esercizio2;
 
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -9,25 +10,44 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class UsersCoupleReducer extends Reducer<Text, Text, Text, Text> {
 	public void reduce(Text key, Iterable<Text> values, Context context)
 			throws IOException, InterruptedException {
-
-		int i=0;
-		Iterator<Text> extIt = values.iterator();
-		while(extIt.hasNext()){
-			int j=0;
-			Text first = new Text();
-			String s = extIt.next().toString()+",";
-			first.set(s);
-			Iterator<Text> intIt = values.iterator();
-			while(j<i+1 && intIt.hasNext()){
-				intIt.next();
-				j+=1;
-			}
-			while(intIt.hasNext()){
-				Text second = intIt.next();
+		
+		List<String> names = new ArrayList<String>();
+		for (Text t : values)
+			names.add(t.toString());
+		int i,j;
+		for (i = 0; i<names.size(); i++){
+			for(j=i+1; j< names.size(); j++){
+				Text first = new Text();
+				Text second = new Text();
+				String f = names.get(i) + ",";
+				first.set(f);
+				second.set(names.get(j));
 				context.write(first, second);
 			}
-			i+=1;
 		}
+		
+
+//		Text second = new Text();
+//		Text first = new Text();
+//		int i=0;
+//		Iterator<Text> extIt = values.iterator();
+//		Iterator<Text> intIt;
+//		while(extIt.hasNext()){
+//			int j=0;
+////			first = new Text();
+//			String s = extIt.next().toString()+",";
+//			first.set(s);
+//			intIt = values.iterator();
+//			while(j<i+1 && intIt.hasNext()){
+//				intIt.next();
+//				j+=1;
+//			}
+//			while(intIt.hasNext()){
+//				second = intIt.next();
+//				context.write(first, second);
+//			}
+//			i+=1;
+//		}
 //		String s = "";
 //		Text finale = new Text();
 //		for (Text t : values){
