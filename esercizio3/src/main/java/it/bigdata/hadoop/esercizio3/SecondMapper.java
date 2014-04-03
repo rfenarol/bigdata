fb654082e7445f0b941d523545ce46eb40e1db7a
octawizard <roberto.manca.89@gmail.com>
2014-04-03 20:52:23 +0200

    fixed tokenizer problem, to test properly

--- esercizio3/src/main/java/it/bigdata/hadoop/esercizio3/SecondMapper.java
+++ esercizio3/src/main/java/it/bigdata/hadoop/esercizio3/SecondMapper.java
@@ -8,20 +8,24 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class SecondMapper extends Mapper<LongWritable, Text, UserCoupleWritable, Text> {
-	private String first;
-	private String second;
+	private String firstAndSecond;
 	private Text interest = new Text();
 
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		StringTokenizer tokenizer;
 		String line = value.toString();
-		tokenizer = new StringTokenizer(line, ", ");
+		tokenizer = new StringTokenizer(line);
 
 		UserCoupleWritable couple;
-		if(tokenizer.hasMoreTokens()) first = tokenizer.nextToken();
-		if(tokenizer.hasMoreTokens()) second = tokenizer.nextToken();
-		couple = new UserCoupleWritable(first,second);
-		if(tokenizer.hasMoreTokens()) interest.set(tokenizer.nextToken());
+		/* primo token: c1,c2 */
+		if(tokenizer.hasMoreTokens()) 
+			firstAndSecond = tokenizer.nextToken();
+		String[] coppia = firstAndSecond.split(",");
+		couple = new UserCoupleWritable(coppia[0],coppia[1]);
+		
+		/* secondo token: interesse */
+		if(tokenizer.hasMoreTokens()) 
+			interest.set(tokenizer.nextToken());
 		context.write(couple, interest);
 
 	}
