1e5a901c3a3f9662127f0045b395be9399d3992b
octawizard <roberto.manca.89@gmail.com>
2014-04-05 12:04:17 +0200

    clean some comments

--- esercizio3/src/main/java/it/bigdata/hadoop/esercizio3/UserCoupleWritable.java
+++ esercizio3/src/main/java/it/bigdata/hadoop/esercizio3/UserCoupleWritable.java
@@ -30,9 +30,6 @@ public class UserCoupleWritable implements WritableComparable<UserCoupleWritable
 		this.u2 = u2;
 	}
 	
-	/* da rivedere
-	 * si puo' definire meglio, magari facendo un equals tra le stringhe, senza usare il String 
-	 */
 	public boolean equals (Object o){
 		if (o instanceof UserCoupleWritable){
 			UserCoupleWritable c = (UserCoupleWritable) o;
